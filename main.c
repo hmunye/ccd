27b49753eab349d7001d722a669623a015c228c6
Huseen <136820327+hmunye@users.noreply.github.com>
2025-03-21 18:55:08 -0400

    added hex output formatting with byte offset and variable byte grouping

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +10,8 @@ typedef enum {
 } Flags;
 
 #define MAX_FLAG_LENGTH 6
-#define DEFAULT_OCTETS_PER_LINE 16
+#define DEFAULT_BYTES_PER_LINE 16
+#define BYTE_SPACING 2 /* Insert space after every 2 bytes */
 
 void print_help(const char *prog_name) {
     printf("Usage:\n");
@@ -27,7 +29,7 @@ int main(int argc, char **argv) {
     }
 
     Flags flags = FLAG_NONE;
-    int oct_per_line = DEFAULT_OCTETS_PER_LINE;
+    int bytes_per_line = DEFAULT_BYTES_PER_LINE;
 
     /* parse arguments */
     for (int i = 1; i < argc; ++i) {
@@ -39,9 +41,9 @@ int main(int argc, char **argv) {
 
         if (strncmp(argv[i], "-c", MAX_FLAG_LENGTH) == 0) {
             if (i + 1 < argc) {
-                oct_per_line = atoi(argv[++i]);
+                bytes_per_line = atoi(argv[++i]);
 
-                if (oct_per_line <= 0) {
+                if (bytes_per_line <= 0) {
                     fprintf(stderr,
                             "ERROR: invalid number for octets per line\n");
                     exit(EXIT_FAILURE);
@@ -61,6 +63,48 @@ int main(int argc, char **argv) {
     if (flags & FLAG_HELP) {
         print_help(argv[0]);
         return EXIT_SUCCESS;
+    }
+
+    char *file_path = argv[argc - 1];
+    FILE *file = fopen(file_path, "r");
+
+    if (!file) {
+        fprintf(stderr, "ERROR: failed to open file %s: %s\n", file_path,
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    char buf[bytes_per_line];
+    size_t bytes_read = 0;
+    size_t byte_offset = 0;
+
+    while ((bytes_read =
+                fread(buf, sizeof(char), (size_t)bytes_per_line, file)) > 0) {
+        printf("%08zx: ", byte_offset);
+
+        for (size_t i = 0; i < bytes_read; i++) {
+            if (i != 0 && i % BYTE_SPACING == 0) {
+                printf(" ");
+            }
+
+            printf("%02x", (unsigned char)buf[i]);
+        }
+
+        printf("\n");
+
+        byte_offset += bytes_read;
+    }
+
+    if (ferror(file)) {
+        fprintf(stderr, "ERROR: failed to read file %s: %s\n", file_path,
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (fclose(file) == EOF) {
+        fprintf(stderr, "ERROR: failed to close file %s: %s\n", file_path,
+                strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
     return EXIT_SUCCESS;
